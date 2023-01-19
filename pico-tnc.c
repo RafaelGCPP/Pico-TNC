bb625f05d632865c7f8bf97b4eabd806dcaaad33
Rafael Pinto <rafaelgcpp@gmail.com>
2023-01-18 22:18:14 -0300

    Preparing demo without IRQ

--- pico-tnc.c
+++ pico-tnc.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/dma.h"
-#include "hardware/interp.h"
-#include "hardware/timer.h"
-#include "pico/cyw43_arch.h"
+#include <pico/stdlib.h>
+#include <pico/cyw43_arch.h>
 
 #include "pwm-audio.h"
 #include "afsk-parameters.h"
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
 
 int main()
 {
@@ -23,24 +15,19 @@ int main()
     setup_pwm_generator();
     setup_pwm_generator_dma_channel();
     
-    
     // Initialize the stdio interface
     
     stdio_init_all();
+    for (uint i = 0; i < AUDIO_BUFFER_SIZE; i++)
+    {
+        audio_buffer[i] = sine200hz[i % 66];
+    }
 
-    // Interpolator example code
-    interp_config cfg = interp_default_config();
-    // Now use the various interpolator library functions for your use case
-    // e.g. interp_config_clamp(&cfg, true);
-    //      interp_config_shift(&cfg, 2);
-    // Then set the config 
-    interp_set_config(interp0, 0, &cfg);
-
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-
-
-    puts("Hello, world!");
-
+    while (1)
+    {
+        puts("Hello, world!");
+        send_audio(AUDIO_BUFFER_SIZE, false);
+        sleep_ms(10000);
+    }
     return 0;
 }
--- pwm-audio.h
+++ pwm-audio.h
@@ -8,7 +8,9 @@
 
 void setup_pwm_generator();
 void setup_pwm_generator_dma_channel();
- 
+void send_audio(uint nsamples, bool irq0);
+
+
 #define AUDIO_BUFFER_SIZE ((32+6)*SAMPLES_PER_BIT)
 
 extern uint32_t audio_buffer[];
